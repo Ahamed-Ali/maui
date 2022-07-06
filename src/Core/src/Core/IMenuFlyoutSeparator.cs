a2a4a673481817a725d2208edbd9179c13b0fee4
Eilon Lipton <Eilon@users.noreply.github.com>
2022-07-06 11:14:27 -0700

    Update APIs and remove unused code

--- src/Core/src/Core/IMenuFlyoutSeparator.cs
+++ src/Core/src/Core/IMenuFlyoutSeparator.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Microsoft.Maui
+﻿namespace Microsoft.Maui
 {
 	public interface IMenuFlyoutSeparator : IMenuFlyoutItem
 	{
