3d7a335f686d71b15ceae8de8732884ac9aa53f4
AhamedAliNishad <ahamedalinishad.j@syncfusion.com>
2025-03-13 22:05:21 +0530

    Fixed the EmptyViewTemplate issues in collectionview

--- src/Controls/src/Core/Handlers/Items/ItemsViewHandler.Windows.cs
+++ src/Controls/src/Core/Handlers/Items/ItemsViewHandler.Windows.cs
@@ -305,28 +305,32 @@ namespace Microsoft.Maui.Controls.Handlers.Items
 			}
 
 			var emptyView = Element.EmptyView;
+			var emptyViewTemplate = Element.EmptyViewTemplate;
 
-			if (emptyView == null)
+			if (emptyViewTemplate is not null)
 			{
-				return;
+				// If EmptyViewTemplate is provided, use it instead of EmptyView
+				_emptyView = RealizeEmptyViewTemplate(emptyView, emptyViewTemplate);
 			}
-
-			switch (emptyView)
+			else if (emptyView is not null)
 			{
-				case string text:
-					_emptyView = new TextBlock
-					{
-						HorizontalAlignment = Microsoft.UI.Xaml.HorizontalAlignment.Center,
-						VerticalAlignment = Microsoft.UI.Xaml.VerticalAlignment.Center,
-						Text = text
-					};
-					break;
-				case View view:
-					_emptyView = RealizeEmptyView(view);
-					break;
-				default:
-					_emptyView = RealizeEmptyViewTemplate(emptyView, Element.EmptyViewTemplate);
-					break;
+				switch (emptyView)
+				{
+					case string text:
+						_emptyView = new TextBlock
+						{
+							HorizontalAlignment = Microsoft.UI.Xaml.HorizontalAlignment.Center,
+							VerticalAlignment = Microsoft.UI.Xaml.VerticalAlignment.Center,
+							Text = text
+						};
+						break;
+					case View view:
+						_emptyView = RealizeEmptyView(view);
+						break;
+					default:
+						_emptyView = RealizeEmptyViewTemplate(emptyView, null); // Fallback
+						break;
+				}
 			}
 
 			(ListViewBase as IEmptyView)?.SetEmptyView(_emptyView, _formsEmptyView);
